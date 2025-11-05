53b4e4b90713a9f689c1d25cd0ecd45369336241
Cesar Rivas <csr_armando96@hotmail.com>
2025-11-05 00:37:05 -0600

    chore: configure env vars validation

---
+++ config/index.ts
@@ -0,0 +1,18 @@
+import env from 'env-var';
+
+if (process.env.NODE_ENV !== 'production') {
+  const { config } = await import('dotenv');
+  config();
+}
+
+export const envs = {
+  PORT: env.get('PORT').required().asPortNumber(),
+  POSTGRES_USER: env.get('POSTGRES_USER').required().asString(),
+  POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').required().asString(),
+  POSTGRES_DB: env.get('POSTGRES_DB').required().asString(),
+  POSTGRES_PORT: env.get('POSTGRES_PORT').required().asPortNumber(),
+  POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
+  PGADMIN_DEFAULT_PASSWORD: env.get('PGADMIN_DEFAULT_PASSWORD').required().asString(),
+  PGADMIN_DEFAULT_EMAIL: env.get('PGADMIN_DEFAULT_EMAIL').required().asString(),
+  NODE_ENV: env.get('NODE_ENV').required().asEnum(['development', 'production', 'test']),
+};
