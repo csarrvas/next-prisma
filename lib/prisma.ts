b38ad668f7c02cc7a3970187149120aad7538805
Cesar Rivas <csr_armando96@hotmail.com>
2025-11-12 14:46:39 -0600

    fix: openssl-3.0.x vercel issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,29 @@
 import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { envs } from '../config';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
+  pool: Pool | undefined;
 };
+
+const pool =
+  globalForPrisma.pool ??
+  new Pool({
+    connectionString: envs.POSTGRES_URL,
+  });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+  globalForPrisma.pool = pool;
+}
